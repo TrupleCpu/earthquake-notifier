08fc8f47d6bd9bb7dbea0f6b532d3eba4f737de3
Janssen Carl Amaya <126572562+TrupleCpu@users.noreply.github.com>
2025-10-18 01:49:40 +0800

    Update route.ts

--- src/app/api/scraper/route.ts
+++ src/app/api/scraper/route.ts
@@ -1,20 +1,16 @@
 import { NextResponse } from "next/server";
-import * as cheerio from "cheerio";
-import axios from "axios";
-import https from "https";
 import redis from "@/lib/redis";
 import EarthquakeLog from "@/app/models/EarthquakeLog";
 import { connectToDatabase } from "@/lib/mongodb";
+import axios from "axios";
 
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
-
 export async function GET() {
   try {
-
-   const cached: string | null = await redis.get("latestEarthquake");
-      if (cached && typeof cached === "string") {
+    const cached: string | null = await redis.get("latestEarthquake");
+    if (cached) {
       try {
         return NextResponse.json(JSON.parse(cached));
       } catch {
@@ -23,51 +19,34 @@ export async function GET() {
       }
     }
 
+    const { data } = await axios.get(
+      "https://phivolc-scraper.janamaya438.workers.dev/",
+      { timeout: 10000 }
+    );
 
+    const latestEarthquake = data.latestEarthquake;
 
-    
-
-    const { data: html } = await axios.get("https://earthquake.phivolcs.dost.gov.ph/", {
-      headers: {
-        "User-Agent":
-          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36",
-      },
-      timeout: 10000,
-    });
-
-    const $ = cheerio.load(html);
-
-    const firstRow = $("table.MsoNormalTable tr:nth-child(2)");
-    const cols = firstRow.find("td");
-
-    if (cols.length < 6) {
-      throw new Error("Table structure changed or missing columns");
+    if (!latestEarthquake) {
+      throw new Error("Worker did not return latest earthquake");
     }
-
-    const latestEarthquake = {
-      dateTime: $(cols[0]).text().trim(),
-      latitude: $(cols[1]).text().trim(),
-      longitude: $(cols[2]).text().trim(),
-      depth: $(cols[3]).text().trim(),
-      magnitude: $(cols[4]).text().trim(),
-      location: $(cols[5]).text().trim(),
-    };
 
     await connectToDatabase();
     const exists = await EarthquakeLog.findOne({ dateTime: latestEarthquake.dateTime });
-    if(!exists){
+    if (!exists) {
       await EarthquakeLog.create(latestEarthquake);
     }
 
-  await redis.set("latestEarthquake", JSON.stringify({ latestEarthquake }), {
-  ex: 30 
-});
+    await redis.set(
+      "latestEarthquake",
+      JSON.stringify({ latestEarthquake }),
+      { ex: 30 }
+    );
 
     return NextResponse.json({ latestEarthquake });
   } catch (error) {
-    console.error("Scraping error:", error);
+    console.error("Fetching error:", error);
     return NextResponse.json(
-      { error: "Failed to scrape PHIVOLCS", details: error },
+      { error: "Failed to fetch latest earthquake", details: error },
       { status: 500 }
     );
   }
