a928c48aa9ed31499717d4a227e4b490cc4a128e
Janssen Carl Amaya <126572562+TrupleCpu@users.noreply.github.com>
2025-10-18 03:10:19 +0800

    Update route.ts

--- src/app/api/scraper/route.ts
+++ src/app/api/scraper/route.ts
@@ -1,5 +1,4 @@
 import { NextResponse } from "next/server";
-import redis from "@/lib/redis";
 import EarthquakeLog from "@/app/models/EarthquakeLog";
 import { connectToDatabase } from "@/lib/mongodb";
 import axios from "axios";
@@ -9,16 +8,6 @@ export const revalidate = 0;
 
 export async function GET() {
   try {
-    const cached: string | null = await redis.get("latestEarthquake");
-    if (cached) {
-      try {
-        return NextResponse.json(JSON.parse(cached));
-      } catch {
-        console.warn("Invalid cached data, clearing...");
-        await redis.del("latestEarthquake");
-      }
-    }
-
     const { data } = await axios.get(
       "https://phivolc-scraper.janamaya438.workers.dev/",
       { timeout: 10000 }
@@ -31,22 +20,18 @@ export async function GET() {
     }
 
     await connectToDatabase();
-    const exists = await EarthquakeLog.findOne({ dateTime: latestEarthquake.dateTime });
+    const exists = await EarthquakeLog.findOne({
+      dateTime: latestEarthquake.dateTime,
+    });
     if (!exists) {
       await EarthquakeLog.create(latestEarthquake);
     }
-
-    await redis.set(
-      "latestEarthquake",
-      JSON.stringify({ latestEarthquake }),
-      { ex: 30 }
-    );
 
     return NextResponse.json({ latestEarthquake });
   } catch (error) {
     console.error("Fetching error:", error);
     return NextResponse.json(
-      { error: "Failed to fetch latest earthquake", details: error },
+      { error: "Failed to fetch latest earthquake", details: error.message },
       { status: 500 }
     );
   }
